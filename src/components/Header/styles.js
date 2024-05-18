5a7599ff9d02ac0ea7ff89416cc296eedafa49df
lfcamargo1 <ulipsz11@gmail.com>
2024-05-17 22:24:55 -0300

    Atualizando arquivos.

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 180px;
+  text-align: center;
+  background: #01977E;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 40px;
+  color: #fff;
+`;
